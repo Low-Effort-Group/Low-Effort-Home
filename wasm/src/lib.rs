ffe60b769f30f3330f643cadd58a8c9a88527909
a-rvid <git@rvid.eu>
2026-03-11 23:05:44 +0100

    I reconsidered and made str_to_bytes available to javascript, we'll see if that affects performance on larger data

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -12,16 +12,12 @@ pub fn bcrypt(password: &str, cost: u32) -> String {
     return bcrypt_hash(password, cost).unwrap();
 }
 
-fn str_to_bytes(s: &str) -> Vec<u8> { // Consider making this available to the javascript side.
+#[wasm_bindgen]
+pub fn str_to_bytes(s: &str) -> Vec<u8> {
     return s.as_bytes().to_vec();
 }
 
 #[wasm_bindgen] 
 pub fn base64(data: &[u8]) -> String { // Might be slower than btoa in smaller data inputs however I have more control this way.
     return general_purpose::STANDARD.encode(data);
-}
-
-#[wasm_bindgen]
-pub fn base64_str(data: &str) -> String { // It's ~1 ms faster than calling str_to_bytes(base64(data)).
-    return general_purpose::STANDARD.encode(str_to_bytes(data));
 }
