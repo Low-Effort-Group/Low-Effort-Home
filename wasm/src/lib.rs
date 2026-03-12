215f118425681da830ecf74d208feab79215e2e7
a-rvid <git@rvid.eu>
2026-03-12 12:45:07 +0000

    encode command

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,8 @@
 use wasm_bindgen::prelude::wasm_bindgen;
-use bcrypt::hash as bcrypt_hash;
 use base64::{engine::general_purpose, Engine as _};
 use sha2::{Sha224, Sha256, Sha384, Sha512, Digest as Sha2};
 use md5::{Md5};
+use bcrypt::hash as bcrypt_hash;
 use hex;
 
 #[wasm_bindgen]
@@ -22,33 +22,13 @@ pub fn base64(data: &[u8]) -> String { // Might be slower than btoa in smaller d
     return general_purpose::STANDARD.encode(data);
 }
 
-// Hash algorithms
 #[wasm_bindgen]
-pub fn bcrypt(password: &str, cost: u32) -> String {
-    return bcrypt_hash(password, cost).unwrap();
-}
-
-#[wasm_bindgen]
-pub fn md5(data: &[u8]) -> String {
-    return hex::encode(Md5::digest(data));
-}
-
-#[wasm_bindgen]
-pub fn sha224(data: &[u8]) -> String {
-    return hex::encode(Sha224::digest(data));
-}
-
-#[wasm_bindgen]
-pub fn sha256(data: &[u8]) -> String {
-    return hex::encode(Sha256::digest(data));
-}
-
-#[wasm_bindgen]
-pub fn sha384(data: &[u8]) -> String {
-    return hex::encode(Sha384::digest(data));
-}
-
-#[wasm_bindgen]
-pub fn sha512(data: &[u8]) -> String {
-    return hex::encode(Sha512::digest(data));
+pub fn encode(data: &[u8], cost: u32) -> Vec<String> {
+    let md5 = hex::encode(Md5::digest(data));
+    let sha224 = hex::encode(Sha224::digest(data));
+    let sha256 = hex::encode(Sha256::digest(data));
+    let sha384 = hex::encode(Sha384::digest(data));
+    let sha512 = hex::encode(Sha512::digest(data));
+    let bcrypt = bcrypt_hash(data, cost).unwrap_or_else(|_| "Error".to_string());
+    return vec![md5, sha224, sha256, sha384, sha512, bcrypt];
 }
