06cd8c9cf7bb50f9c9975b5251b827dc9b7b561c
a-rvid <git@rvid.eu>
2026-03-12 19:33:08 +0100

    remove according to me unnecessary sha224 and sha384+RIPEMD

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use base64::{engine::general_purpose, Engine as _};
-use sha2::{Sha224, Sha256, Sha384, Sha512, Digest as Sha2};
+use sha1::Sha1;
+use sha2::{Sha256, Sha512, Digest as Sha2};
 use md5::{Md5};
 use bcrypt::hash as bcrypt_hash;
 use hex;
@@ -25,9 +26,8 @@ pub fn base64(data: &[u8]) -> String { // Might be slower than btoa in smaller d
 #[wasm_bindgen]
 pub fn encode(data: &[u8], cost: u32, file: bool) -> Vec<String> {
     let md5 = hex::encode(Md5::digest(data));
-    let sha224 = hex::encode(Sha224::digest(data));
+    let sha1 = hex::encode(Sha1::digest(data));
     let sha256 = hex::encode(Sha256::digest(data));
-    let sha384 = hex::encode(Sha384::digest(data));
     let sha512 = hex::encode(Sha512::digest(data));
     let bcrypt: String;
 
@@ -37,5 +37,5 @@ pub fn encode(data: &[u8], cost: u32, file: bool) -> Vec<String> {
         bcrypt = "Bcrypt not available in file context".to_string();
     }
 
-    return vec![md5, sha224, sha256, sha384, sha512, bcrypt];
+    return vec![md5, sha1, sha256, sha512, bcrypt];
 }
