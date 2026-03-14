9873aeb16c33429e9302cd99be701a06fbcb5ef4
a-rvid <git@rvid.eu>
2026-03-14 12:00:57 +0100

    crypto

--- rust/crypto/src/lib.rs
+++ rust/crypto/src/lib.rs
@@ -1,5 +1,4 @@
 use wasm_bindgen::prelude::wasm_bindgen;
-use base64::{engine::general_purpose, Engine as _};
 use sha1::Sha1;
 use sha2::{Sha256, Sha512, Digest as Sha2};
 use md5::{Md5};
@@ -15,12 +14,6 @@ use hex;
 #[wasm_bindgen]
 pub fn str_to_bytes(s: &str) -> Vec<u8> {
     return s.as_bytes().to_vec();
-}
-
-// Encoding
-#[wasm_bindgen] 
-pub fn base64(data: &[u8]) -> String { // Might be slower than btoa in smaller data inputs however I have more control this way.
-    return general_purpose::STANDARD.encode(data);
 }
 
 #[wasm_bindgen]
